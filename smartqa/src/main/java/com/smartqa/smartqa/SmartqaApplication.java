4a17a88216cac4389dbc1aeb610a6bbc069eff56
马乾坤 <18839460508@163.com>
2026-05-27 10:14:51 +0800

    初始化提交
    初始化提交

---
+++ smartqa/src/main/java/com/smartqa/smartqa/SmartqaApplication.java
@@ -0,0 +1,13 @@
+package com.smartqa.smartqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartqaApplication.class, args);
+	}
+
+}
---
+++ smartqa/src/test/java/com/smartqa/smartqa/SmartqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartqa.smartqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
